35010a42054280499a59213bb9a3db59253ec991
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-06-09 20:14:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,1 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Contutorialuctor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
-
-const titleCased = (str) => {
- 
-  str.map(ele => {
-     ele = ele.split(" ")
-    ele = ele[0].toUpperCase() + ele.substr(1)
-  })
-  return str.join(" ")
-}
-console.log(titleCased(tutorials))
-
+const tutorials = [  'what does the this keyword mean?',  'What is the Contutorialuctor OO pattern?',  'implementing Blockchain Web API',  'The Test Driven Development Workflow',  'What is NaN and how Can we Check for it',  'What is the difference between stopPropagation and preventDefault?',  'Immutable State and Pure Functions',  'what is the difference between == and ===?',  'what is the difference between event capturing and bubbling?',  'what is JSONP?'];const titleCased = () => {    let newarr = tutorials.map((title)=>{    let words = title.split(" ");        words = words.map(word=>{      return word[0].toUpperCase()+ word.substr(1, word.length);    })    return words.join(" ")      })   return newarr;   // return tutorials}titleCased()
